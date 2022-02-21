ec44ef4b28c3b8545be9139486856c03055d60b4
Zuza Napiórkowska <napiorkowskaz@gmail.com>
2022-02-21 16:25:52 +0100

    add axios

--- app.js
+++ app.js
@@ -2,22 +2,34 @@ fetch("http://api.tvmaze.com/shows/6771/episodebynumber?season=1&number=1")
     .then(res => {
         console.log("RESPONSE, WAITING TO PARSE...", res)
         return res.json();
-    }) .then(data => {
+    }).then(data => {
         console.log(data, "parsed data")
         console.log(data.name);
-    }) .catch(err => {
+    }).catch(err => {
         console.log(err, "this is the error message");
     })
 
+//.................................................................................................
 
-    const fetchTitle = async () => {
-        try {
+const fetchTitle = async () => {
+    try {
         const res = await fetch("http://api.tvmaze.com/shows/6771/episodebynumber?season=1&number=1")
         const data = await res.json();
         console.log(data.name);
     } catch (err) {
         console.log("Something went wrong", err);
     }
-    }
+}
 
-    fetchTitle();
+fetchTitle();
+
+//.................................................................................................
+
+
+axios.get("http://api.tvmaze.com/shows/6771/episodebynumber?season=1&number=1")
+    .then(res => {
+        console.log(res, "axios parsed data")
+        console.log(res.data.name);
+    }).catch(err => {
+        console.log(err, "oops, axios error")
+    })
