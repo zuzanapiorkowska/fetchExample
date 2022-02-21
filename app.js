2c466c67a134550a99af05437b62a6bfe87780b0
Zuza Napiórkowska <napiorkowskaz@gmail.com>
2022-02-21 16:09:40 +0100

    add async function

--- app.js
+++ app.js
@@ -3,7 +3,21 @@ fetch("http://api.tvmaze.com/shows/6771/episodebynumber?season=1&number=1")
         console.log("RESPONSE, WAITING TO PARSE...", res)
         return res.json();
     }) .then(data => {
-        console.log(data, "this is clg from the second then")
+        console.log(data, "parsed data")
+        console.log(data.name);
     }) .catch(err => {
         console.log(err, "this is the error message");
     })
+
+
+    const fetchTitle = async () => {
+        try {
+        const res = await fetch("http://api.tvmaze.com/shows/6771/episodebynumber?season=1&number=1")
+        const data = await res.json();
+        console.log(data.name);
+    } catch (err) {
+        console.log("Something went wrong", err);
+    }
+    }
+
+    fetchTitle();
