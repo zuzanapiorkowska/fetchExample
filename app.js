19fe1291cc79721ae1f7ec85916bd5266a7c6321
Zuza Napiórkowska <napiorkowskaz@gmail.com>
2022-02-21 18:51:48 +0100

    init

--- app.js
+++ app.js
@@ -1,73 +1,30 @@
-fetch("http://api.tvmaze.com/shows/6771/episodebynumber?season=1&number=1")
-    .then(res => {
-        console.log("RESPONSE, WAITING TO PARSE...", res)
-        return res.json();
-    }).then(data => {
-        console.log(data, "parsed data")
-        console.log(data.name);
-    }).catch(err => {
-        console.log(err, "this is the error message");
-    })
+const input = document.getElementById("input");
+const button = document.getElementById("button");
+const root = document.getElementById("root");
 
-//.................................................................................................
-
-const fetchTitle = async () => {
+const getTVShows = async () => {
     try {
-        const res = await fetch("http://api.tvmaze.com/shows/6771/episodebynumber?season=1&number=1")
-        const data = await res.json();
-        console.log(data.name);
+        const searchTerm = input.value
+        const config = {params: {q: searchTerm}}
+        const res = await axios.get("https://api.tvmaze.com/search/shows?q=", config)
+        const TVShows = res.data;
+        console.log(TVShows);
+        TVShows.forEach(TVShow => {
+            if (TVShow.show.image) {
+                const newImg = document.createElement("img");
+                const imgSrc = TVShow.show.image.medium;
+                newImg.src = imgSrc;
+                root.append(newImg);
+            }
+        });
+
+
     } catch (err) {
-        console.log("Something went wrong", err);
+        console.log(err, "oops error");
     }
 }
 
-fetchTitle();
-
-//.................................................................................................
-
-axios.get("http://api.tvmaze.com/shows/6771/episodebynumber?season=1&number=1")
-    .then(res => {
-        console.log(res, "axios parsed data")
-        console.log(res.data.name);
-    }).catch(err => {
-        console.log(err, "oops, axios error")
-    })
-
-//.................................................................................................
-
-const fetchTitleWithAxios = async () => {
-    try {
-        const res = await axios.get("http://api.tvmaze.com/shows/6771/episodebynumber?season=1&number=1");
-        console.log(res.data.name);
-    } catch (err) {
-        console.log("async axios error", err);
-    }
-}
-
-fetchTitleWithAxios();
-
-//.....................................................................................................
-
-const jokes = document.getElementById("jokes");
-const button = document.getElementById("jokes-button")
-
-const getDadJoke = async () => {
-    try {
-        const config = { headers: { Accept: "application/json" } }
-        const res = await axios.get("https://icanhazdadjoke.com/", config)
-        return res.data.joke;
-    } catch (err) {
-        console.log(err);
-    }
-}
-
-const addNewJoke = async () => {
-        const newJoke = await getDadJoke();
-        const newLi = document.createElement("li");
-        newLi.append(newJoke);
-        jokes.append(newLi);
-}
-
-button.addEventListener("click", addNewJoke)
-//.....................................................................................................
-
+button.addEventListener("click", () => {
+    root.innerHTML=""
+    getTVShows();
+})
